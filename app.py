652a0824e684db243bf0a1f8959dc796ceaee15e
sepehr mousaviyan <semomos3@users.noreply.huggingface.co>
2023-07-06 18:40:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from nbformat import read, NO_CONVERT
 
 # Load movie data from CSV file
 @st.cache
-def load_movie_data():
+def load_movie_data(file_path):
     movie_data = pd.read_csv('movies_metadata.csv')
     return movie_data['name']
 
@@ -21,10 +21,24 @@ def recommend(favorite_movie):
 def main():
 
     st.title("Movie Recommender System")
-    
-    # Load movie data
-    movie_data = load_movie_data()
 
+    folder_path = st.text_input("Enter the folder path")
+    file_name = st.text_input("Enter the file name (including .csv)")
+    
+    if folder_path and file_name:
+        try:
+            # Construct the file path
+            file_path = folder_path.rstrip("/") + "/" + file_name
+
+            movie_data = load_movie_data(file_path_path)
+
+            # Display DataFrame
+            st.write(df)
+
+        except FileNotFoundError:
+            st.error("File not found. Please check the folder path and file name.")
+    # Load movie data
+    # movie_data = load_movie_data(folder_path)
 
     # # User input
     # favorite_movie = st.multiselect("Select your favorite movie", movie_data['movie'])
