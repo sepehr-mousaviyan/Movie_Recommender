60751049433904586bdf1eef1384343b9f6b7ec9
sepehr mousaviyan <semomos3@users.noreply.huggingface.co>
2023-07-06 19:40:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import os
-
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 from nbformat import read, NO_CONVERT
 
 # Load movie data from CSV file
@@ -14,12 +15,37 @@ def load_movie_data(file_path):
 
 # Recommender function
 def recommend(favorite_movie):
-    notebook_path = 'recommender.ipynb'
+    notebook_path = 'Recommender.ipynb'
     notebook = read(open(notebook_path), NO_CONVERT)
 
     exec('\n'.join([cell['source'] for cell in notebook['cells'] if cell['cell_type'] == 'code']), globals())
 
     return recommend(favorite_movie)
+
+
+def execute_notebook_sections(notebook_path, sections):
+    # Read the notebook
+    with open(notebook_path, "r", encoding="utf-8") as f:
+        notebook = nbformat.read(f, as_version=4)
+    
+    # Create a preprocessor to execute the notebook
+    preprocessor = ExecutePreprocessor(timeout=600)
+
+    # Iterate over the notebook cells
+    for cell in notebook.cells:
+        if cell.cell_type == "code":
+            cell_metadata = cell.metadata
+
+            # Execute the cell if it has the desired section metadata
+            if "section" in cell_metadata and cell_metadata["section"] in sections:
+                preprocessor.preprocess_cell(cell, {"metadata": cell_metadata})
+
+    # Save the executed notebook
+    with open("executed_notebook.ipynb", "w", encoding="utf-8") as f:
+        nbformat.write(notebook, f)
+
+
+
 
 def main():
 
@@ -48,7 +74,7 @@ def main():
     movie_data = load_movie_data(file_path)
 
     # Display DataFrame
-    st.write(movie_data)
+    # st.write(movie_data)
     
     # Load movie data
 
@@ -56,11 +82,16 @@ def main():
     # # User input
     favorite_movie = st.multiselect("Select your favorite movie", movie_data)
 
-    # if st.button("Get Recommendations"):
-    #     recommendations = recommend(favorite_movie)
-    #     st.write("Recommended Movies:")
-    #     for movie in recommendations:
-    #         st.write(movie)
+    if st.button("Get Recommendations"):
+
+        notebook_path = "recommender.ipynb"
+        sections_to_execute = ["Import required libaries", "Content based"]
+        
+        execute_notebook_sections(notebook_path, sections_to_execute)
+        recommendations = get_recommendations(favorite_movie).head(5)
+        # st.write("Recommended Movies:")
+        # for movie in recommendations:
+        #     st.write(movie)
 
 if __name__ == "__main__":
     main()
