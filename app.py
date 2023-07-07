006c9ad1e47bf68d1a58a6dfec7aadff4d2e490b
sepehr mousaviyan <semomos3@users.noreply.huggingface.co>
2023-07-07 13:05:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def recomend(movie):
         if (moviei == movie):
             movie_index = count
             st.text(movie_index)
-            return
+            break
     # movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
