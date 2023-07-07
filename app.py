56a2f8914f083e23220fb517b3aeee6a652ecc04
sepehr mousaviyan <semomos3@users.noreply.huggingface.co>
2023-07-07 11:36:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,35 @@ from nbformat import read, NO_CONVERT
 import pickle
 import requests
 
-# Load movie data from CSV file
+
 @st.cache_data 
 def load_movie_data(file_path):
     movie_data = pd.read_csv('movies_metadata.csv')
     movie_data = movie_data.sort_values('title') 
     return movie_data['title']
 
+def fetch_poster(movie_id):
+    #Using leaked key
+    response = requests.get('https://api.themoviedb.org/3/movie/{0}?api_key=d61204e42321cddd34e0f954c0bf649f&language=en-US'.format(movie_id))
+    data = response.json()
+    return  "https://image.tmdb.org/t/p/w500/"+ data['poster_path']
+
 # Recommender function
-def recommend(favorite_movie):
-    
+def recomend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    recomendedMovies = []
+    recomended_movies_posters = []
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recomendedMovies.append(movies.iloc[i[0]].title)
+        recomended_movies_posters.append(fetch_poster(movie_id))
+    return  recomendedMovies, recomended_movies_posters
 
-    return "1","2","3"
-
-movies = pickle.load(open("movies.pkl", 'rb'))
-movies_date = list( movies['release_date'])
-movies_list=list(movies['title'] )
-movies_list = [i+' '+str(j.year) for i,j in zip(movies_list,movies_date)]
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
 
 
 def main():
@@ -37,18 +49,28 @@ def main():
 
     st.title("Movie Recommender System")
 
-    file_path = "movie_metadata.csv"
-    
-    # st.write(file_path)
-    movie_data = load_movie_data(file_path)
 
-    favorite_movie = st.multiselect("Select your favorite movies(at least 3 movies)", movie_data)
+    selected = st.multiselect("Select your favorite movies(at least 3 movies)", movies['title'].values)
 
     if st.button("Get Recommendations"):
-        recommendations = recommend(favorite_movie)
+        recomend_movies, poster = recomend(selected_movie)
         st.write("Recommended Movies:[Just made for you:)]")
-        for movie in recommendations:
-            st.write(movie)
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.text(recomend_movies[0])
+            st.image(poster[0])
+        with col2:
+            st.text(recomend_movies[1])
+            st.image(poster[1])
+        with col3:
+            st.text(recomend_movies[2])
+            st.image(poster[2])
+        with col4:
+            st.text(recomend_movies[3])
+            st.image(poster[3])
+        with col5:
+            st.text(recomend_movies[4])
+            st.image(poster[4])
 
 if __name__ == "__main__":
     main()
